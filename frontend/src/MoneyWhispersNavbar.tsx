21ed5ad4bf22978e84167126aba26cec3959464a
Sneha Rampalli <svrampalli@gmail.com>
2025-04-22 20:53:10 -0400

    Cleanup

--- frontend/src/MoneyWhispersNavbar.tsx
+++ frontend/src/MoneyWhispersNavbar.tsx
@@ -4,9 +4,7 @@ import HomePage from './pages/HomePage.tsx';
 import MoneyVibeCheckTest from './pages/MoneyVibeCheckTest.tsx';
 import AnonPosts from './pages/AnonPosts.tsx';
 import {Container, Nav, Navbar} from 'react-bootstrap'
-import Users from './pages/Users.tsx';
 import Login from './pages/Login.tsx';
-import Logout from './pages/Logout.tsx';
 
 const MoneyWhispersNavbar = () => {
   return (
@@ -20,8 +18,9 @@ const MoneyWhispersNavbar = () => {
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/money-vibe-check-test">Money Vibe Check Test</Nav.Link>
             <Nav.Link as={Link} to="/posts">Anonymous Money Posts</Nav.Link>
-            <Nav.Link as={Link} to="/users">Create User</Nav.Link>
-            <Nav.Link as={Logout}>Logout</Nav.Link>
+          </Nav>
+          <Nav className="justify-content-end">
+            <Nav.Link as={Login}></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -30,7 +29,6 @@ const MoneyWhispersNavbar = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/money-vibe-check-test" element={<MoneyVibeCheckTest />} />
         <Route path="/posts" element={<AnonPosts />} />
-        <Route path="/users" element={<Users />} />
     </Routes>
     </Router>
 );
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import {Button, Form, Card, Modal} from 'react-bootstrap';
+import {Button, Form, Modal} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 type User = {
     username: string;
@@ -21,19 +22,19 @@ const Login = () => {
     const [success, setSuccess] = useState<boolean>(false);
     const [show, setShow] = useState(false);
 
+    const [showLoginForm, setShowLoginForm] = useState<boolean>(true);
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleLoginShow = () => setShow(true);
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         const { name, value } = e.target;
         setPostData(prev => ({ ...prev, [name]: value }));
     };
 
-
     useEffect(() => {
         setPostData(prev => ({ ...prev }));
     }, []);
 
-    const handleSubmit = async (e: React.FormEvent) => {
+    const handleLoginSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setLoading(true);
         setError(null);
@@ -75,17 +76,94 @@ const Login = () => {
         }
     };
 
+    const [logoutError, setLogoutError] = useState<string | null>(null);
+
+    const handleLogout = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${backendURL}/logout`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+            });
+
+            if (!response.ok) {
+                let logoutErrorMessage = 'Failed to logout.';
+                try {
+                    const logoutErrorData = await response.json();
+                    logoutErrorMessage = logoutErrorData.error || logoutErrorMessage;
+                } catch (parseError) {
+                }
+                throw new Error(logoutErrorMessage);
+            }
+
+            const result = await response.json();
+        } catch (err: any) {
+            setLogoutError(err.message || 'An unexpected error occurred.');
+        } finally {
+            console.log("logged out")
+        }
+    }
+
+    const handleCreateUserSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+        setSuccess(false);
+
+        try {
+            const response = await fetch(`${backendURL}/create-user`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(postData),
+            });
+
+            if (!response.ok) {
+                let errorMessage = 'Failed to create user.';
+                try {
+                    const errorData = await response.json();
+                    errorMessage = errorData.error || errorMessage;
+                } catch (parseError) {
+                }
+                throw new Error(errorMessage);
+            }
+
+            const result = await response.json();
+            setSuccess(true);
+            setPostData({ 
+                username: '',
+                password: '',
+            });
+        } catch (err: any) {
+            setError(err.message || 'An unexpected error occurred.');
+        } finally {
+            // loading
+            setLoading(false);
+            // close modal
+            setShow(false);
+        }
+    };
+
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
-                Login
-            </Button>
+            <div className="loginButton">
+                <Button variant="primary" className="loginButton" onClick={handleLoginShow}>
+                    Login
+                </Button>
+                <Button variant="outline-primary" className="loginButton" onClick={handleLogout}>
+                    Logout
+                </Button>
+            </div>
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                <Modal.Title>Login</Modal.Title>
+                <Modal.Title>{showLoginForm ? 'Login' : 'Create account'}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form onSubmit={handleSubmit} className="space-y-4">
+                    {showLoginForm && <Form onSubmit={handleLoginSubmit} className="space-y-4">
                         <Form.Group>
                             <Form.Label htmlFor="username" className="block text-sm font-medium">Username</Form.Label>
                             <Form.Control
@@ -111,13 +189,56 @@ const Login = () => {
                                 disabled={loading}
                             />
                         </Form.Group>
-                    </Form>
+                        <br/>
+                        <Form.Text>Don't have an account. Create one <Button onClick={() => setShowLoginForm(false)}>here</Button>.</Form.Text>
+                    </Form>}
+                    {!showLoginForm && 
+                        <Form onSubmit={handleCreateUserSubmit} className="space-y-4">
+                            <Form.Group>
+                                <Form.Label htmlFor="username" className="block text-sm font-medium">Username</Form.Label>
+                                <Form.Control
+                                    id="username"
+                                    name="username"
+                                    value={postData.username}
+                                    onChange={handleChange}
+                                    placeholder="Enter your username"
+                                    className="mt-1"
+                                    required
+                                    disabled={loading}
+                                />
+                                <Form.Label htmlFor="password" className="block text-sm font-medium">Password</Form.Label>
+                                <Form.Control
+                                    id="password"
+                                    name="password"
+                                    value={postData.password}
+                                    onChange={handleChange}
+                                    placeholder="Enter your password"
+                                    className="mt-1"
+                                    required
+                                    type='password'
+                                    disabled={loading}
+                                />
+                                <Form.Label htmlFor="email" className="block text-sm font-medium">Email</Form.Label>
+                                <Form.Control
+                                    id="email"
+                                    name="email"
+                                    value={postData.email}
+                                    onChange={handleChange}
+                                    placeholder="Enter your email"
+                                    className="mt-1"
+                                    disabled={loading}
+                                />
+                            </Form.Group>
+                            <br/>
+                            <Form.Text>Have an account? Login <Button onClick={() => setShowLoginForm(true)}>here</Button>.</Form.Text>
+                        </Form>
+                    }
                 </Modal.Body>
                 <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
                     Close
                 </Button>
-                <Button type="submit" disabled={loading} onClick={handleSubmit}>
+                <Button type="submit" disabled={loading} onClick={showLoginForm ? handleLoginSubmit : handleCreateUserSubmit}>
                     {loading ? 'Creating...' : 'Login'}
                 </Button>
                 </Modal.Footer>
